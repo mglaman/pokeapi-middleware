9f0bf2cb09f33480c39212a98ae3a53581748d53
Matt Glaman <nmd.matt@gmail.com>
2020-09-11 15:55:04 -0500

    phpstan fixes

--- src/PokemonMaster.php
+++ src/PokemonMaster.php
@@ -10,8 +10,19 @@ use React\Stream\WritableResourceStream;
 
 final class PokemonMaster
 {
+    /**
+     * @var \React\EventLoop\LoopInterface
+     */
     private $loop;
+
+    /**
+     * @var \React\Http\Browser
+     */
     private $client;
+
+    /**
+     * @var \Clue\React\NDJson\Encoder
+     */
     private $artifact;
 
     public function __construct(LoopInterface $loop)
@@ -23,38 +34,41 @@ final class PokemonMaster
         $this->artifact = new Encoder($stream, JSON_UNESCAPED_SLASHES);
     }
 
-    private function normalizePokemon(object $pokemon, object $species)
+    /**
+     * @return array<string, mixed>
+     */
+    private function normalizePokemon(\stdClass $pokemon, \stdClass $species): array
     {
         return [
             'id' => $pokemon->name,
             'order' => $pokemon->order,
             'image' => $pokemon->sprites->front_default,
-            'types' => array_map(static function (object $type) {
+            'types' => array_map(static function (\stdClass $type) {
                 return $type->type->name;
             }, $pokemon->types),
-            'name' => array_map(static function (object $text) {
+            'name' => array_map(static function (\stdClass $text): array {
                 return [
                     'value' => $text->name,
                     'langcode' => $text->language->name
                 ];
-            }, array_values(array_filter($species->names, static function (object $text) {
-                return in_array($text->language->name, ['en', 'es']);
+            }, array_values(array_filter($species->names, static function (\stdClass $text): bool {
+                return in_array($text->language->name, ['en', 'es'], true);
             }))),
-            'description' => array_map(static function (object $text) {
+            'description' => array_map(static function (\stdClass $text): array {
                 return [
                     'value' => $text->flavor_text,
                     'langcode' => $text->language->name,
                 ];
-            }, array_values(array_filter($species->flavor_text_entries, static function (object $text) {
-                return $text->version->name === 'y' && in_array($text->language->name, ['en', 'es']);
+            }, array_values(array_filter($species->flavor_text_entries, static function (\stdClass $text): bool {
+                return $text->version->name === 'y' && in_array($text->language->name, ['en', 'es'], true);
             }))),
-            'genus' => array_map(static function (object $text) {
+            'genus' => array_map(static function (\stdClass $text): array {
                 return [
                     'value' => $text->genus,
                     'langcode' => $text->language->name
                 ];
-            }, array_values(array_filter($species->genera, static function (object $text) {
-                return in_array($text->language->name, ['en', 'es']);
+            }, array_values(array_filter($species->genera, static function (\stdClass $text): bool {
+                return in_array($text->language->name, ['en', 'es'], true);
             }))),
             'baby' => $species->is_baby,
             'legendary' => $species->is_legendary,
@@ -62,16 +76,16 @@ final class PokemonMaster
         ];
     }
 
-    private function fetchPokemon(object $result)
+    private function fetchPokemon(\stdClass $result): void
     {
-        $this->loop->futureTick(function () use ($result) {
+        $this->loop->futureTick(function () use ($result): void {
             print "\033[32m[http]\033[0m Fetching {$result->url}" . PHP_EOL;
-            $this->client->get($result->url)->then(function (ResponseInterface $response) {
+            $this->client->get($result->url)->then(function (ResponseInterface $response): void {
                 $pokemon = \json_decode((string)$response->getBody());
 
                 $species_url = $pokemon->species->url;
                 print "\033[32m[http]\033[0m Fetching {$species_url}" . PHP_EOL;
-                $this->client->get($species_url)->then(function (ResponseInterface $response) use ($pokemon) {
+                $this->client->get($species_url)->then(function (ResponseInterface $response) use ($pokemon): void {
                     $species = \json_decode((string) $response->getBody());
                     $this->artifact->write($this->normalizePokemon($pokemon, $species));
                 });
@@ -79,13 +93,13 @@ final class PokemonMaster
         });
     }
 
-    private function fetchPokemonList(string $url)
+    private function fetchPokemonList(string $url): void
     {
-        $this->loop->futureTick(function () use ($url) {
+        $this->loop->futureTick(function () use ($url): void {
             print "\033[32m[http]\033[0m Fetching {$url}" . PHP_EOL;
             $this->client
             ->get($url)
-            ->then(function (ResponseInterface $response) {
+            ->then(function (ResponseInterface $response): void {
                 $body = \json_decode((string)$response->getBody());
 
                 if ($body->next !== null) {
